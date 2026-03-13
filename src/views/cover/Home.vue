73e284bbdcd317ad629deee77ac953069bb84fc0
Sandi <sandidwi367@gmail.com>
2026-03-14 00:42:09 +0700

    initial commit

---
+++ src/views/cover/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container-fluid p-0">
+    <div class="home">
+      <div class="row">
+        <div class="col-md-12">
+          <p class="head-title">we are getting married</p>
+          <h1>Ahmad</h1>
+          <div class="row" style="justify-content: center; align-items: center">
+            <div class="col-md-3 ps-0">
+              <svg width="60" height="12" viewBox="0 0 60 12" fill="none" class="text-white">
+                <path d="M0 6 L60 6" stroke="currentColor" stroke-width="3" />
+              </svg>
+            </div>
+            <div class="col-md-2">
+              <span class="connect">&</span>
+            </div>
+            <div class="col-md-3 ps-0">
+              <svg width="60" height="12" viewBox="0 0 60 12" fill="none" class="text-white">
+                <path
+                  d="M0 6 L60 6"
+                  stroke="currentColor"
+                  stroke-width="3"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+          <h1>Sari</h1>
+          <p class="tgl-pelaksana">25.08.2026</p>
+          <p class="terhormat">Kepada Yth. Bapak/Ibu/Saudara/i</p>
+          <p class="undangan">Tamu Undangan</p>
+          <button
+            type="button"
+            class="btn"
+            style="
+              backdrop-filter: blur(3px);
+              background-color: rgba(255, 255, 255, 0.2);
+              color: white;
+            "
+          >
+            Buka Undangan
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
