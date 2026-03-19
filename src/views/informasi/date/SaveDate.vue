003abb13e970d49390567dc39cc2d2546e1d9d0f
Sandi <sandidwi367@gmail.com>
2026-03-19 23:04:46 +0700

    checking

---
+++ src/views/informasi/date/SaveDate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="save-date">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <p>counting down</p>
+          <h1>save the date</h1>
+          <div class="row pt-5 pb-5" style="justify-content: center; align-items: center">
+            <div class="col-3 pe-0" style="text-align: end">
+              <svg width="60" height="12" viewBox="0 0 60 12" fill="none" style="color: #a3b18a">
+                <path d="M0 6 L60 6" stroke="currentColor" stroke-width="3" />
+              </svg>
+            </div>
+            <div class="col-2" style="width: 42px">
+              <span
+                ><img src="../../../assets/img/icon-park-solid_like.svg" alt="love" srcset=""
+              /></span>
+            </div>
+            <div class="col-3 ps-0" style="text-align: start">
+              <svg width="60" height="12" viewBox="0 0 60 12" fill="none" style="color: #a3b18a">
+                <path
+                  d="M0 6 L60 6"
+                  stroke="currentColor"
+                  stroke-width="3"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
